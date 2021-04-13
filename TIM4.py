ef6afa4f53a247c4da4138f555903d720980ae88
paskalwolski <53813349+paskalwolski@users.noreply.github.com>
2021-04-13 09:41:02 +0200

    OOP Refinement Removed main class - it is just  a  main method now Started updating  menu actions Issues with the parameters for menu actions - will have to be rethought Added some formatting for ease of reading

--- TIM4.py
+++ TIM4.py
@@ -10,122 +10,143 @@
 
 #Imports
 import os
-#import  HAP
+#import  HAP   #Coming Soon
 
-class Main:
-    def __init__(self):
-        #Class Variable Declaration for each instance of the program (Should  only be  1)
-        self.track = None
-        self.lesson_name = None
-        self.lesson_number = None
-        self.cwd = None
-        self.menu_options = None
 
-        #Class Variable Definition
-        self.loc = os.getcwd()
-        return None
+# class Main():
+    # Is this necessary? Might not need to have 'main' class
+    # def __init__(self):
+    #     #Class Variable Declaration for each instance of the program (Should  only be  1)
+    #     self.track = None
+    #     self.lesson_name = None
+    #     self.lesson_number = None
+    #     self.cwd = None
+    #     self.menu_options = None
+
+    #     #Class Variable Definition
+    #     self.loc = os.getcwd()
+    #     return None
     
     #clear command line
-    def clear(self):
-        os.system('cls')
+
+
+track = None
+
+#Main Thread
+#######################################
+
+def main():
+    clear()
+    print("Welcome to TIM4!")
+    menu = (("Access Google Drive", find_google_drive), ("Something Else I guess", on_close)) #Rethink this
+    menu_action(menu, main)
+    
+    
+    # k = input()
+    # if k=='1':
+    #     print("Starting Card Creation")
+    #     find_google_drive('G:') #Default drive mount point for windows
+    # elif k=='2':
+    #     print("Doing something else...")
+    # else:
+    #     clear()
+    #     unexpected_action()
+    #     main()
+
+    # return None
+
+
+# Grab input to perform menu_action based on the menu_actions dict
+def menu_action(menu, catch):
+    #Catch: Object to run on catch. Usually the method from which it is called. 
+    for i in range(len(menu)):
+        print("{}. {}".format(i+1, menu[i][0]))
+        
+    print("\n------------------------------\n")
+    action = input()
+    try:
+        clear()
+        menu[int(action)-1]()
+    except IndexError:
+        unexpected_action()
+        catch()
+    except Exception as e:
+        print(e)
+        unexpected_action()
+        catch()
+
+
+# Search for Google Drive at G:
+# If not found, search for drive name ('Google Drive')
+def find_google_drive(volume):
+    try:
+        os.chdir(volume)
+        print("Found Google Drive")
+        input()
         return None
 
-    #Main Thread
-    #Also used to restart program on dead end
-    def main(self):
-        self.clear()
-        print("Welcome to TIM4!")
-        print("1. Create Lesson Cards\n2. Something else I guess")
-        print("\n--------------------------\n")
+    except FileNotFoundError:
+        clear()
+        print("Google Drive is not mounted at G:")
+        print("Running Backup Search")
+        check_mount_point()
 
-        #Should use:
-        # menu_action(input())
-        k = input()
-        if k=='1':
-            print("Starting Card Creation")
-            self.find_google_drive()
-        elif k=='2':
-            print("Doing something else...")
-        else:
-            self.clear()
-            print("Your response was not one of the expected responses")
-            input("Enter to continue")
-            self.main()
+    return None
 
-        return None
 
-    # Grab input to perform menu_action based on the menu_actions dict`
-    def menu_action(self, menu, catch):
-        print(menu["text"])
-        action  = input("Enter the Option Number")
-        if action in menu["menu_options"]:
-            self.clear()
-            print(action)
-            menu["menu_options"[action]]() #TODO: Fix  this  line
-            return None
-        else:
-            self.unexpected_action()
-            catch()
-
-        return None
-
-    # Search for Google Drive at D:
-    # If not found, search for drive name ('Google Drive')
-    def find_google_drive(self):
-        try:
-            os.chdir('K:')
-            print("Found Google Drive")
-            input()
-            return None
-
-        except FileNotFoundError:
-            self.clear()
-            print("Google Drive is not mounted at G:")
-            print("Running Backup Search")
-            self.check_mount_point()
-
-        return None
-    # Get User  input to find Google Drive Volume
-    def check_mount_point(self):
-        mount_point = input("Please enter the Volume Label that Google Drive is mounted at:\n")
-        try:
-            os.chdir(mount_point +  ':')
-            self.clear()
-            print("Found Google Drive")
-        except FileNotFoundError:
-            self.clear()
-            print("\'" + mount_point + ":\' does not exist")
-            menu  =  {
-                'text' : "1. Try a new Volume\n2. Exit",
-                "menu_options" : {
-                    '1': self.check_mount_point,
-                    '2': self.on_close
-                }
+# Get User  input to find Google Drive Volume
+def check_mount_point():
+    mount_point = input("Please enter the Volume Label that Google Drive is mounted at:\n")
+    try:
+        os.chdir(mount_point +  ':')
+        clear()
+        print("Found Google Drive")
+    except FileNotFoundError:
+        clear()
+        print("\'" + mount_point + ":\' does not exist")
+        menu  =  {
+            'text' : "1. Try a new Volume\n2. Exit",
+            "menu_options" : {
+                '1': check_mount_point,
+                '2': on_close
             }
-            self.menu_action(menu, self.check_mount_point)
-            # menu_action = input("1. Try a New Volume\n2. Exit")
-            # if menu_action == '1':
-            #     self.check_mount_point()
-            #     return None
-            # elif menu_action == '2':
-            #     self.on_close()
-            #     return  None
-            # else:
-            #     self.unexpected_action()
-            #     self.check_mount_point()
+        }
+        menu_action(menu,check_mount_point)
+        # menu_action = input("1. Try a New Volume\n2. Exit")
+        # if menu_action == '1':
+        #     self.check_mount_point()
+        #     return None
+        # elif menu_action == '2':
+        #     self.on_close()
+        #     return  None
+        # else:
+        #     self.unexpected_action()
+        #     self.check_mount_point()
 
-                
+            
+#Utility Functions for use from main method
+#######################################
 
-    # Function declared for cleanup later
-    def on_close(self):
-        print("\n\nClosing Session\n\n")
-        quit()
-
-    def unexpected_action(self):
-        self.clear()
-        print("Your response was not expected\n___________________")
+def clear():
+    os.system('cls')
+    return None
 
 
+def on_close():
+    print("\n\nClosing Session\n\n")
+    quit()
+
+
+def unexpected_action():
+    clear()
+    print("Your response was not expected\n___________________")
+
+
+#######################################
+
+
+#Lesson Class - Instantiate with Current Working Lesson
+#######################################
 class Lesson:
     def __init__(self,  track, checkpoint,  lesson_name):
 
@@ -135,6 +156,4 @@ class Lesson:
 
 # Running the Code
 if __name__=="__main__":
-    main =  Main()
-    main.main()
-    main.on_close()
+    main()
